8459b51e4646be8aae74da9a2b329f577aeed1f7
Felipe Mendes <fmendes@totvs.com.br>
2024-06-17 15:25:27 -0300

    chore: using different tables from database

--- tests/process_embedding.py
+++ tests/process_embedding.py
@@ -9,5 +9,9 @@ if __name__ == "__main__":
     similarity_finder = SimilarityFinder()
     product = 'Datasul'
     module = ''
-    sentence = 'subject'
-    similarity_finder.process_data(product, module, sentence)
+    sentence_source = 'subject'
+    use_product = False
+    use_module = False
+    table_name = 'tickets_embeddings_chunks'
+    threshold = 65
+    similarity_finder.process_data(table_name, sentence_source, use_product, use_module, threshold)
